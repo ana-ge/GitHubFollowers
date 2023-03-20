5742119266c07b8cdccf353d6bd6658bda3f7334
ana-ge <ge.anatoliy@gmail.com>
2023-03-20 23:06:19 +0900

    Add GFButton and GFTextField for our buttons and placeholders

---
+++ GitHubFollowers/GFTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GFTextField.swift
+//  GitHubFollowers
+//
+//  Created by Анатолий Ге on 20.03.2023.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label // white text in dark mode, black text in white mode
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a username"
+    }
+    
+
+}
