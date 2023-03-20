ae1e62f541a9a642678cd14c1dc573e7e7150b37
ana-ge <ge.anatoliy@gmail.com>
2023-03-20 22:24:12 +0900

    Add our Navigation Controllers, viewControllers and assets for our app

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Анатолий Ге on 19.03.2023.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+
+    }
+}
