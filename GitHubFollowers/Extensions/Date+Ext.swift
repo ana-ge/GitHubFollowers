991b03e86d27604e5e00095e8be3ecfa499cdc2e
ana-ge <ge.anatoliy@gmail.com>
2023-03-28 00:06:22 +0900

    Adding DateLabel with DateFormatter

---
+++ GitHubFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension Date {
+    
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        
+        return dateFormatter.string(from: self)
+    }
+    
+}
