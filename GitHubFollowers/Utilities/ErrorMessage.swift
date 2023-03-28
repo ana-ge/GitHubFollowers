5b97675bb1b0884ca46b4f7aecbe5327afe88c39
ana-ge <ge.anatoliy@gmail.com>
2023-03-28 23:32:25 +0900

    Update for ErrorMessage

--- GitHubFollowers/Utilities/ErrorMessage.swift
+++ GitHubFollowers/Utilities/ErrorMessage.swift
@@ -5,4 +5,6 @@ enum GFError: String, Error {
     case unableToComplete = "Unable to complete your request. Please check your internet connection"
     case invalidResponse = "Invalid response from the server. Pleasy try again."
     case invalidData = "The data receiver from the server was invalid. Please try again."
+    case unableToFavorite = "There was an error to add this user to favorites. Please try again."
+    case alreadyInFavorites = "You've already favorited this user."
 }
