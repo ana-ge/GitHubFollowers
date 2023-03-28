36f724f2ccfe889b4a263e24589e14ca57ec9227
ana-ge <ge.anatoliy@gmail.com>
2023-03-28 22:34:43 +0900

    Adding configureActionButton

--- GitHubFollowers/Controllers/FollowerListVC.swift
+++ GitHubFollowers/Controllers/FollowerListVC.swift
@@ -1,5 +1,10 @@
 import UIKit
 
+protocol FollowerListVCDelegate: AnyObject {
+    
+    func didRequestFollowers(for username: String)
+}
+
 class FollowerListVC: UIViewController {
     
     enum Section {
@@ -117,6 +122,7 @@ extension FollowerListVC: UICollectionViewDelegate {
         
         let destinationVC = UserInfoVC()
         destinationVC.username = follower.login
+        destinationVC.delegate = self
         let navController = UINavigationController(rootViewController: destinationVC)
         present(navController, animated: true)
     }
@@ -135,4 +141,21 @@ extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
         isSearching = false
         updateData(on: followers)
     }
+}
+
+extension FollowerListVC: FollowerListVCDelegate {
+    
+    func didRequestFollowers(for username: String) {
+        // get followers for that use
+        self.username = username
+        title = username
+        page = 1
+        followers.removeAll()
+        filteredFollowers.removeAll()
+        collectionView.setContentOffset(.zero, animated: true)
+        getFollowers(username: username, page: page)
+    }
+    
+    
+    
 }
