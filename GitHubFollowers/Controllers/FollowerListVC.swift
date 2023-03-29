66bb621040d8a2579326efb06f25bdb63fe33cd8
ana-ge <ge.anatoliy@gmail.com>
2023-03-29 22:51:36 +0900

    Adding addButton in our Navigation controller

--- GitHubFollowers/Controllers/FollowerListVC.swift
+++ GitHubFollowers/Controllers/FollowerListVC.swift
@@ -39,6 +39,9 @@ class FollowerListVC: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
     }
     
     func configureCollectionView() {
@@ -98,6 +101,10 @@ class FollowerListVC: UIViewController {
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
         DispatchQueue.main.async { self.dataSource.apply(snapshot) }
+    }
+    
+    @objc func addButtonTapped() {
+        print("Add button tapped")
     }
 }
 
