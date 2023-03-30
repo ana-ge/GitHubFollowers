33ce9d92ac16ea7b80f80065618591791b33b682
ana-ge <ge.anatoliy@gmail.com>
2023-03-30 21:51:01 +0900

    Adding swipe to delete function

--- GitHubFollowers/Views/Cells/FavoriteCell.swift
+++ GitHubFollowers/Views/Cells/FavoriteCell.swift
@@ -37,10 +37,6 @@ class FavoriteCell: UITableViewCell {
             usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
             usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
             usernameLabel.heightAnchor.constraint(equalToConstant: 40)
-        
-        
-        
-        
         ])
     }
     
