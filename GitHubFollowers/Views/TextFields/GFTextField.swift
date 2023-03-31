dc0b1108434bf05efb23aeceaee279d4562d2d96
ana-ge <ge.anatoliy@gmail.com>
2023-03-31 21:20:37 +0900

    Refactoring SceneDelegate

--- GitHubFollowers/Views/TextFields/GFTextField.swift
+++ GitHubFollowers/Views/TextFields/GFTextField.swift
@@ -30,6 +30,7 @@ class GFTextField: UITextField {
         backgroundColor = .tertiarySystemBackground
         autocorrectionType = .no
         returnKeyType = .go
+        clearButtonMode = .whileEditing // When we typed smth x-button appears
         placeholder = "Enter a username"
     }
     
