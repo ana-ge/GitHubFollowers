1b66b14b1d7cf1a34a80fbf3d73955760f836fc8
ana-ge <ge.anatoliy@gmail.com>
2023-04-03 19:43:15 +0900

    Refactored UIView extension

--- GitHubFollowers/Extensions/UIVIewController+Ext.swift
+++ GitHubFollowers/Extensions/UIVIewController+Ext.swift
@@ -3,14 +3,21 @@ import SafariServices
 
 extension UIViewController {
     
-    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
-        DispatchQueue.main.async {
-            
-            let alertVC = GFALertVC(title: title, message: message, buttonTitle: buttonTitle)
-            alertVC.modalPresentationStyle = .overFullScreen
-            alertVC.modalTransitionStyle = .crossDissolve
-            self.present(alertVC, animated: true)
-        }
+    func presentGFAlert(title: String, message: String, buttonTitle: String) {
+        let alertVC = GFALertVC(title: title, message: message, buttonTitle: buttonTitle)
+        alertVC.modalPresentationStyle = .overFullScreen
+        alertVC.modalTransitionStyle = .crossDissolve
+        present(alertVC, animated: true)
+    }
+    
+    
+    func presentDefaultError() {
+        let alertVC = GFALertVC(title: "Something went wrong",
+                                message: "We were unable to complete your task at this time. Please try again",
+                                buttonTitle: "Ok")
+        alertVC.modalPresentationStyle = .overFullScreen
+        alertVC.modalTransitionStyle = .crossDissolve
+        present(alertVC, animated: true)
     }
     
     
