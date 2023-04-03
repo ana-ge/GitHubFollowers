d2678732ceb9f46194f5927789b61bf3c3709cf3
ana-ge <ge.anatoliy@gmail.com>
2023-04-03 19:38:39 +0900

    Refactored Date extension

--- GitHubFollowers/Extensions/Date+Ext.swift
+++ GitHubFollowers/Extensions/Date+Ext.swift
@@ -3,11 +3,16 @@ import Foundation
 extension Date {
     
     func convertToMonthYearFormat() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMM yyyy"
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        
-        return dateFormatter.string(from: self)
+        return formatted(.dateTime.month().year())
     }
-    
 }
+
+
+// The old version of DateFormatter
+//    func convertToMonthYearFormat() -> String {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "MMM yyyy"
+//        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+//
+//        return dateFormatter.string(from: self)
+//    }
